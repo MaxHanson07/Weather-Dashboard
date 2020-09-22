c9afb10fbbc1fe948bf20d2f779627884eef8c49
Max Hanson <mthanson2@gmail.com>
2020-09-21 17:23:04 -0700

    Added pseudo code to js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert("linked");
+// Takes user input "city" and makes ajax call to weather api for both current conditions and 5-day forecast
+
+// Display city name, date, weather condition icon, temperature humidity, wind speed, and uv index in .current-forecast
+// Must create elements
+
+// Change background color of UV index depending on if conditions are favorable, moderate, or severe
+// Green for favorable, yellow for moderate, red for severe
+
+// Display date, weather condition icon, temperature, and humidity in .five-day-forecast 
+
+// Save searches to local storage, create each city to a button, and append it to page in .search-history
+// Only display last 8
+
+// Display the last search on the page even after refresh
+
+// Add CSS to newly created elements (Five day forecast boxes)
